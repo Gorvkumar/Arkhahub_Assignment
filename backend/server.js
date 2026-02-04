8b3aee47f8e8d95738881af9cdfbf7093ab2bfc6
Gorvkumar <94596904+Gorvkumar@users.noreply.github.com>
2026-02-04 12:17:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ const app = require('./src/app');
 const config = require('./src/config/config');
 
 const PORT = config.SERVER_PORT || 5000;
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "EnergyGrid Backend is running 🚀"
+  });
+});
+
 
 app.listen(PORT, () => {
   console.log(`EnergyGrid Backend Server running on http://localhost:${PORT}`);
